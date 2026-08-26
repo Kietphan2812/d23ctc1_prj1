f9071a60947796342d34bc5b320e21e80cb37c5c
synguyen <chisy.nguyen@gmail.com>
2026-08-26 10:56:24 +0700

    Add Student clas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using prj1.Model;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Student student1 = new Student(1, "Alice", 20);
+        Student student2 = new Student(2, "Bob", 22);
+
+        student1.DisplayStudentInfo();
+        student2.DisplayStudentInfo();
+    }
+}   
