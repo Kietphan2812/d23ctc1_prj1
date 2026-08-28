dfaf75e6861b7284b60dd488f2863aac5f12b518
synguyen <chisy.nguyen@gmail.com>
2026-08-28 16:30:04 +0700

    Phát triển lớp StudentManager (close #13)

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using prj1.Model;
+using prj1.Service;
 public class Program
 {
     public static void Main(string[] args)
     {
+        StudentManager studentManager = new StudentManager();
         Student student1 = new Student(1, "Alice", 20);
         Student student2 = new Student(2, "Bob", 22);
 
-        student1.DisplayStudentInfo();
-        student2.DisplayStudentInfo();
+        studentManager.AddStudent(student1);
+        studentManager.AddStudent(student2);
+        studentManager.DisplayAllStudents();
+
     }
 }   
